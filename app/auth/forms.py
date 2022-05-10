e6935af792297fd725097b21a9beffb148863247
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-05-10 07:53:45 +0300

    add WTF forms to aid in validation

---
+++ app/auth/forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from ..models import User
+
+class LoginForm(FlaskForm):
+    """
+    Login form
+    """
+    email = EmailField('Email',validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    remember = BooleanField('Remember Me')
+
+
+class SignupForm(FlaskForm):
+    """
+    Signup form
+    """
+    email = EmailField('Email',validators=[InputRequired()])
+    username = StringField('username',validators=[InputRequired()])
+    name = StringField('Name',validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    about = TextAreaField('About')
+
+    def validate_email(self,data_field):
+            if User.query.filter_by(email = data_field.data).first():
+                raise ValidationError('Email already exists')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('Username is already taken')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,8 @@
-from crypt import methods
-from turtle import title
 from flask import render_template, redirect, url_for, flash, request
-from app.auth import bp
 from . import auth
 from .. import db
+from app.models import User
+from app.auth.forms import LoginForm, SignupForm
 
 user = User()
 
@@ -26,3 +25,24 @@ def login():
     title = 'Login'
 
     return render_template('auth/login.html', title=title, form=form )
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data,
+                    password=form.password.data, name=form.name.data, role_id=2)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to Pitches","email/welcome_user",user.email,user=user)
+        return redirect(request.args.get('next') or url_for('main.index'))
+    title = "New Account"
+    return render_template('signup.html', form=form, title=title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    """Function that handles logout"""
+    logout_user()
+    return redirect(url_for("main.index"))
